298a2823a8262245f1dc2ace8baaeffc3a6ffa9a
deshmukh612 <99964682+deshmukh612@users.noreply.github.com>
2022-05-30 16:41:30 +0530

    updated

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -15,26 +15,29 @@ import {
 const AddProduct = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
+  // const initialRef = React.useRef()
+  // const finalRef = React.useRef()
+
   return (
     <>
       <Button my={4} data-cy="add-product-button" onClick={onOpen}>
         Add New Product
       </Button>
-      <Modal isOpen={isOpen} >
+      <Modal  isOpen={isOpen}  onClose={onClose}>
         <ModalBody pb={6}>
           <Input data-cy="add-product-title" />
           <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+            <option data-cy="add-product-category-shirt">shirt</option>
+            <option data-cy="add-product-category-pant">pant</option>
+            <option data-cy="add-product-category-jeans">jeans</option>
           </Select>
           <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+            <Radio data-cy="add-product-gender-male">male</Radio>
+            <Radio data-cy="add-product-gender-female">female</Radio>
+            <Radio data-cy="add-product-gender-unisex">unisex</Radio>
           </RadioGroup>
           <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button">Submit</Button>
+          <Button data-cy="add-product-submit-button" onClick={onClose}>Submit</Button>
         </ModalBody>
       </Modal>
     </>
